c4e3351bdcc03da095892ebed877f418692725c4
skytower144 <skytower144@daum.net>
2026-07-25 16:34:35 +0900

    chore: Return to square one

--- Assets/Scripts/UI/SelectableUI.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public abstract class SelectableUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
-{
-    public abstract void OnPointerClick(PointerEventData eventData);
-    public abstract void OnPointerEnter(PointerEventData eventData);
-    public abstract void OnPointerExit(PointerEventData eventData);
-}
